34f40242697de3a3d9c6a0ed2e708f8eb7b1855f
Kyle Nguyen <kylenguyencs@gmail.com>
2023-07-26 16:00:45 -0700

    Add: Form for user info and input error checking

---
+++ src/calculator/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+
+
+class UserInfo(forms.Form):
+    age = forms.IntegerField(label='Age', min_value=0)
+    gender = forms.ChoiceField(label='Gender', choices=[('male', 'Male'), ('female', 'Female')])
+    weight_kg = forms.DecimalField(label='Weight (kg)', min_value=0)
+    height_cm = forms.DecimalField(label='Height (cm)', min_value=0)
+    activity_level = forms.ChoiceField(
+        label='Activity Level',
+        choices=[
+            ('level_1', 'Sedentary: little or no exercise'),
+            ('level_2', 'Exercise 1-3 times/week'),
+            ('level_3', 'Exercise 4-5 times/week'),
+            ('level_4', 'Daily exercise or intense exercise 3-4 times/week'),
+            ('level_5', 'Intense exercise 6-7 times/week'),
+            ('level_6', 'Very intense exercise daily, or physical job'),
+        ]
+    )
