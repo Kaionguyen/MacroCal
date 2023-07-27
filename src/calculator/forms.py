8e4b41c683a68e3a192a5efa83b6573312de562e
Kyle Nguyen <kylenguyencs@gmail.com>
2023-07-26 17:54:00 -0700

    Add: Parameters for user input

--- src/calculator/forms.py
+++ src/calculator/forms.py
@@ -14,6 +14,15 @@ class UserInfo(forms.Form):
             ('level_3', 'Exercise 4-5 times/week'),
             ('level_4', 'Daily exercise or intense exercise 3-4 times/week'),
             ('level_5', 'Intense exercise 6-7 times/week'),
-            ('level_6', 'Very intense exercise daily, or physical job'),
-        ]
-    )
+            ('level_6', 'Very intense exercise daily, or physical job')])
+
+    # weight_goal = forms.ChoiceField(
+    #     label='Weight Goal',
+    #     choices=[
+    #         ('maintain', 'Maintain weight'),
+    #         ('mildlose', 'Mild weight loss'),
+    #         ('weightlose', 'Weight loss'),
+    #         ('extremelose', 'Extreme weight loss'),
+    #         ('mildgain', 'Mild weight gain'),
+    #         ('weightgain', 'Weight gain'),
+    #         ('extremegain', 'Extreme weight gain')])
