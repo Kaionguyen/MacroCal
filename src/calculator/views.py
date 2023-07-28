e06a1697f1053b0fbe4336aa296fd6454bed5040
Kyle Nguyen <kylenguyencs@gmail.com>
2023-07-27 17:17:40 -0700

    Add: Post request, form check, and parameters for API call

--- src/calculator/views.py
+++ src/calculator/views.py
@@ -1,5 +1,33 @@
+import requests
 from django.shortcuts import render
+from django.conf import settings
+from .forms import UserInfo
 
 
 def nutrition_data(request):
-    pass
+    form = UserInfo()
+
+    if request.method == 'POST':
+        form = UserInfo(request.POST)
+
+        if form.is_valid():
+            age = form.cleaned_data['age']
+            gender = form.cleaned_data['gender']
+            weight_kg = form.cleaned_data['weight_kg']
+            height_cm = form.cleaned_data['height_cm']
+            activity_level = form.cleaned_data['activity_level']
+
+            url = 'https://fitness-calculator.p.rapidapi.com/dailycalorie'
+
+            headers = {
+                "X-RapidAPI-Key": settings.API_KEY,
+                "X-RapidAPI-Host": "fitness-calculator.p.rapidapi.com"
+            }
+
+            params = {
+                'age': age,
+                'gender': gender,
+                'weight_kg': weight_kg,
+                'height_cm': height_cm,
+                'activitylevel': activity_level,
+            }
