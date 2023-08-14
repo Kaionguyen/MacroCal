4453361d12a7d59f07366fe4fa1c12eed4756701
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-14 16:48:25 -0700

    Add: Two seperate model forms for diff units

--- src/calculator/forms.py
+++ src/calculator/forms.py
@@ -1,37 +1,79 @@
+from decimal import Decimal
 from django.contrib.auth.models import User
+from calculator.models import UserStat
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm
 from django import forms
 
 
-class MacroCal(forms.Form):
-    age = forms.IntegerField(label='Age', min_value=0, max_value=80)
-    sex = forms.ChoiceField(label='Sex', choices=[('male', 'Male'), ('female', 'Female')])
-    weight_kg = forms.DecimalField(label='Weight (kg)', min_value=40, max_value=160, required=False)
-    height_cm = forms.DecimalField(label='Height (cm)', min_value=130, max_value=230, required=False)
-    weight_lb = forms.DecimalField(label='Weight (lb)', min_value=88.2, max_value=352.7, required=False)
-    height_ft = forms.IntegerField(label='Height (ft)', min_value=4, max_value=7, required=False)
-    height_in = forms.DecimalField(label='Height (in)', min_value=0, max_value=12.00, required=False)
-    activity_level = forms.ChoiceField(
-        label='Activity Level',
-        choices=[
-            ('1', 'BMR'),
-            ('2', 'Sedentary: little or no exercise'),
-            ('3', 'Exercise 1-3 times/week'),
-            ('4', 'Exercise 4-5 times/week'),
-            ('5', 'Daily exercise or intense exercise 3-4 times/week'),
-            ('6', 'Intense exercise 6-7 times/week'),
-            ('7', 'Very intense exercise daily, or physical job')])
+class MetricForm(ModelForm):
+    class Meta:
+        model = UserStat
+        fields = ('age', 'sex', 'weight_kg', 'height_cm', 'activity_level', 'weight_goal')
 
-    weight_goal = forms.ChoiceField(
-        label='Weight Goal',
-        choices=[
-            ('maintain', 'Maintain weight'),
-            ('mildlose', 'Mild weight loss'),
-            ('weightlose', 'Weight loss'),
-            ('extremelose', 'Extreme weight loss'),
-            ('mildgain', 'Mild weight gain'),
-            ('weightgain', 'Weight gain'),
-            ('extremegain', 'Extreme weight gain')])
+    def clean(self):
+        cleaned_data = super().clean()
+
+        weight_kg = cleaned_data.get('weight_kg')
+        height_cm = cleaned_data.get('height_cm')
+
+        if weight_kg is not None:
+            weight_lb = round(weight_kg * Decimal('2.20462'), 2)
+            cleaned_data['weight_lb'] = weight_lb
+
+        if height_cm is not None:
+            height_ft = int(height_cm // Decimal('30.48'))
+            height_in = round((height_cm % Decimal('30.48')) / Decimal('2.54'), 2)
+            cleaned_data['height_ft'] = height_ft
+            cleaned_data['height_in'] = height_in
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.weight_lb = self.cleaned_data['weight_lb']
+        instance.height_ft = self.cleaned_data['height_ft']
+        instance.height_in = self.cleaned_data['height_in']
+
+        if commit:
+            instance.save()
+
+        return instance
+
+
+class UsForm(ModelForm):
+    class Meta:
+        model = UserStat
+        fields = ('age', 'sex', 'weight_lb', 'height_ft', 'height_in', 'activity_level', 'weight_goal')
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        weight_lb = cleaned_data.get('weight_lb')
+        height_ft = cleaned_data.get('height_ft')
+        height_in = cleaned_data.get('height_in')
+
+        if weight_lb is not None:
+            weight_kg = round(weight_lb / Decimal('2.20462'), 2)
+            cleaned_data['weight_kg'] = weight_kg
+
+        if height_ft is not None and height_in is not None:
+            height_cm = round((height_ft * Decimal('30.48')) + (height_in * Decimal('2.54')), 2)
+            cleaned_data['height_cm'] = height_cm
+
+        return cleaned_data
+
+    def save(self, commit=True):
+        instance = super().save(commit=False)
+
+        instance.weight_kg = self.cleaned_data['weight_kg']
+        instance.height_cm = self.cleaned_data['height_cm']
+
+        if commit:
+            instance.save()
+
+        return instance
 
 
 class SignUp(UserCreationForm):
