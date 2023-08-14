496e261047e9d90000076d06001b2c7120e6129d
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-13 17:36:44 -0700

    Add: typescript

--- src/calculator/static/calculator/js/account.js
+++ src/calculator/static/calculator/js/account.js
@@ -1,26 +1,27 @@
-const toLogin = document.getElementById('switch-to-login');
-const toSignup = document.getElementById('switch-to-signup');
-const heading = document.getElementById('dynamic-header');
-const loginSection = document.getElementById('login-section');
-const signupSection = document.getElementById('signup-section');
-
+var toLogin = document.getElementById('switch-to-login');
+var toSignup = document.getElementById('switch-to-signup');
+var heading = document.getElementById('dynamic-header');
+var loginSection = document.getElementById('login-section');
+var signupSection = document.getElementById('signup-section');
 function showSection(sectionId) {
-  if (sectionId === 'loginSection') {
-    signupSection.style.display = 'none';
-    heading.textContent = 'Sign In';
-    loginSection.style.display = 'flex';
-  } else {
-    loginSection.style.display = 'none';
-    heading.textContent = 'Sign Up';
-    signupSection.style.display = 'inline';
-  }
+    if (sectionId === 'loginSection') {
+        signupSection.style.display = 'none';
+        heading.textContent = 'Sign In';
+        loginSection.style.display = 'flex';
+    }
+    else {
+        loginSection.style.display = 'none';
+        heading.textContent = 'Sign Up';
+        signupSection.style.display = 'inline';
+    }
 }
-
-toSignup.addEventListener('click', function() {
-  showSection('signupSection');
-});
-
-toLogin.addEventListener('click', function() {
-  showSection('loginSection');
-});
-
+function handleSignupClick() {
+    showSection('signupSection');
+}
+function handleLoginClick() {
+    showSection('loginSection');
+}
+if (toLogin && toSignup && heading && loginSection && signupSection) {
+    toSignup.addEventListener('click', handleSignupClick);
+    toLogin.addEventListener('click', handleLoginClick);
+}
---
+++ src/calculator/static/calculator/js/account.ts
@@ -0,0 +1,30 @@
+const toLogin = document.getElementById('switch-to-login') as HTMLElement;
+const toSignup = document.getElementById('switch-to-signup') as HTMLElement;
+const heading = document.getElementById('dynamic-header') as HTMLElement;
+const loginSection = document.getElementById('login-section') as HTMLElement;
+const signupSection = document.getElementById('signup-section') as HTMLElement;
+
+function showSection(sectionId: string) {
+  if (sectionId === 'loginSection') {
+    signupSection.style.display = 'none';
+    heading.textContent = 'Sign In';
+    loginSection.style.display = 'flex';
+  } else {
+    loginSection.style.display = 'none';
+    heading.textContent = 'Sign Up';
+    signupSection.style.display = 'inline';
+  }
+}
+
+function handleSignupClick() {
+  showSection('signupSection');
+}
+
+function handleLoginClick() {
+  showSection('loginSection');
+}
+
+if (toLogin && toSignup && heading && loginSection && signupSection) {
+  toSignup.addEventListener('click', handleSignupClick);
+  toLogin.addEventListener('click', handleLoginClick);
+}
