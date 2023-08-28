46f50e533e4d10c997bb0132538451695a99398b
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-28 05:44:00 -0700

    Add: Url for edit profile view

--- src/calculator/urls.py
+++ src/calculator/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.landing_page, name="home"),
     path("profile/<int:pk>/", views.profile, name="profile"),
+    path("edit_profile/<int:pk>/", views.edit_profile, name="edit_profile"),
     path("metric/", views.metric, name="metric"),
     path("imperial/", views.imperial, name="imperial"),
     path("macrocal/", views.calculate_macros, name="macrocal"),
