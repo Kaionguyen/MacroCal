ea329a819e00eeb3708537043e02e2d69e477219
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-29 12:21:02 -0700

    Refactor: The huge refactor. Also added stat-form to results page for unregistered users.

--- src/calculator/views.py
+++ src/calculator/views.py
@@ -7,6 +7,127 @@ from .forms import ImperialForm, MetricForm, SignUp
 from calculator.models import UserStat, Diet, MacroDistribution
 
 
+def retrieve_macros(form_data):
+    url = "https://fitness-calculator.p.rapidapi.com/macrocalculator"
+    headers = {
+        "X-RapidAPI-Key": settings.API_KEY,
+        "X-RapidAPI-Host": "fitness-calculator.p.rapidapi.com",
+    }
+    response = requests.get(url, headers=headers, params=form_data)
+    response_data = response.json()["data"]
+    return response_data
+
+
+def calculate_macros(request, form_choice):
+    if request.method == "POST":
+        user = request.user
+        authenticated = user.is_authenticated
+        stat_instance = None
+
+        if authenticated:
+            try:
+                stat_instance = UserStat.objects.get(user=request.user)
+            except UserStat.DoesNotExist:
+                pass
+
+        form = form_choice(request.POST, instance=stat_instance) if stat_instance and authenticated else form_choice(request.POST)
+
+        if form.is_valid():
+            form_data = {
+                "age": form.cleaned_data["age"],
+                "gender": form.cleaned_data["sex"],
+                "weight": form.cleaned_data["weight_kg"],
+                "height": form.cleaned_data["height_cm"],
+                "activitylevel": form.cleaned_data["activity_level"],
+                "goal": form.cleaned_data["weight_goal"],
+            }
+
+            try:
+                response_data = retrieve_macros(form_data)
+
+                balanced_data = response_data['balanced']
+                lowcarb_data = response_data['lowcarbs']
+                lowfat_data = response_data['lowfat']
+                highprotein_data = response_data['highprotein']
+
+                if not authenticated:
+                    form_type = request.POST.get("form_type")
+
+                    if form_type == "metric":
+                        metric_form = form
+                        imperial_form = ImperialForm()
+                    elif form_type == "imperial":
+                        imperial_form = form
+                        metric_form = MetricForm()
+                    else:
+                        metric_form = MetricForm()
+                        imperial_form = ImperialForm()
+
+                    context = {
+                        "macros": response_data,
+                        "metric_form": metric_form,
+                        "imperial_form": imperial_form,
+                    }
+
+                    return render(request, "calculator/profile.html", context)
+
+                elif authenticated and stat_instance:
+                    stat_instance = form.save()
+                    diet = Diet.objects.get(stats=stat_instance)
+                    diet.calorie = round(response_data['calorie'])
+                    diet.save(update_fields=['calorie'])
+
+                    macro_names = ['balanced', 'lowcarbs', 'lowfat', 'highprotein']
+                    macro_distribution = MacroDistribution.objects.filter(user_diet=diet)
+
+                    for i, macro in enumerate(macro_distribution):
+                        data = response_data[macro_names[i]]
+                        macro.protein = round(data['protein'])
+                        macro.carbs = round(data['carbs'])
+                        macro.fat = round(data['fat'])
+                        macro.save(update_fields=['protein', 'carbs', 'fat'])
+
+                    macros = {
+                        "calorie": diet.calorie,
+                        "balanced": MacroDistribution.objects.get(plan_name='Balanced', user_diet=diet),
+                        "lowcarbs": MacroDistribution.objects.get(plan_name='Low Carb', user_diet=diet),
+                        "lowfat": MacroDistribution.objects.get(plan_name='Low Fat', user_diet=diet),
+                        "highprotein": MacroDistribution.objects.get(plan_name='High Protein', user_diet=diet),
+                    }
+
+                else:
+                    stat_instance = form.save(commit=False)
+                    stat_instance.user = user
+                    stat_instance.save()
+
+                    diet = Diet.objects.create(
+                        calorie=round(response_data['calorie']),
+                        stats=stat_instance,
+                    )
+
+                    macros = {
+                        "calorie": response_data['calorie'],
+                        "balanced": MacroDistribution.objects.create(plan_name='Balanced', **balanced_data, user_diet=diet),
+                        "lowcarbs": MacroDistribution.objects.create(plan_name='Low Carb', **lowcarb_data, user_diet=diet),
+                        "lowfat": MacroDistribution.objects.create(plan_name='Low Fat', **lowfat_data, user_diet=diet),
+                        "highprotein": MacroDistribution.objects.create(plan_name='High Protein', **highprotein_data, user_diet=diet),
+                    }
+
+                    return render(request, "calculator/profile.html", {"macros": macros})
+
+            except requests.exceptions.RequestException as e:
+                error_message = str(e)
+                return render(
+                    request, "calculator/error.html", {"error_message": error_message}
+                )
+
+    else:
+        form = form_choice()
+        context = {"form": form}
+
+        return render(request, "calculator/home.html", context)
+
+
 def landing_page(request):
     metric_form = MetricForm()
     imperial_form = ImperialForm()
@@ -15,10 +136,9 @@ def landing_page(request):
     if request.user.is_authenticated:
         try:
             stat_instance = UserStat.objects.get(user=request.user)
+            return redirect("profile", pk=stat_instance.pk)
         except UserStat.DoesNotExist:
-            stat_instance = None
-    else:
-        stat_instance = None
+            pass
 
     context = {
         "metric_form": metric_form,
@@ -26,10 +146,7 @@ def landing_page(request):
         "signup_form": signup_form,
     }
 
-    if stat_instance:
-        return redirect("profile", pk=stat_instance.pk)
-    else:
-        return render(request, "calculator/home.html", context)
+    return render(request, "calculator/home.html", context)
 
 
 def profile(request, pk):
@@ -60,126 +177,6 @@ def metric(request):
 
 def imperial(request):
     return calculate_macros(request, ImperialForm)
-
-
-def calculate_macros(request, form_choice):
-    if request.method == "POST":
-        if request.user.is_authenticated:
-            try:
-                stat_instance = UserStat.objects.get(user=request.user)
-                form = form_choice(request.POST, instance=stat_instance)
-            except UserStat.DoesNotExist:
-                stat_instance = None
-                form = form_choice(request.POST)
-        else:
-            stat_instance = None
-            form = form_choice(request.POST)
-
-        if form.is_valid():
-            url = "https://fitness-calculator.p.rapidapi.com/macrocalculator"
-
-            headers = {
-                "X-RapidAPI-Key": settings.API_KEY,
-                "X-RapidAPI-Host": "fitness-calculator.p.rapidapi.com",
-            }
-
-            params = {
-                "age": form.cleaned_data["age"],
-                "gender": form.cleaned_data["sex"],
-                "weight": form.cleaned_data["weight_kg"],
-                "height": form.cleaned_data["height_cm"],
-                "activitylevel": form.cleaned_data["activity_level"],
-                "goal": form.cleaned_data["weight_goal"],
-            }
-
-            try:
-                response = requests.get(url, headers=headers, params=params)
-                response_data = response.json()
-                response_data = response_data['data']
-
-                if stat_instance:
-                    form.save()
-
-                    balanced_data = response_data['balanced']
-                    lowcarb_data = response_data['lowcarbs']
-                    lowfat_data = response_data['lowfat']
-                    highprotein_data = response_data['highprotein']
-
-                    diet = Diet.objects.get(stats=UserStat.objects.get(user=request.user))
-                    diet.calorie = response_data['calorie']
-                    diet.save(update_fields=['calorie'])
-
-                    macro_distribution = MacroDistribution.objects.filter(user_diet=diet)
-                    for macro in macro_distribution:
-                        if macro.plan_name == 'Balanced':
-                            macro.protein = balanced_data['protein']
-                            macro.carbs = balanced_data['carbs']
-                            macro.fat = balanced_data['fat']
-                            macro.save(update_fields=['protein', 'carbs', 'fat'])
-                        elif macro.plan_name == 'Low Carb':
-                            macro.protein = lowcarb_data['protein']
-                            macro.carbs = lowcarb_data['carbs']
-                            macro.fat = lowcarb_data['fat']
-                            macro.save(update_fields=['protein', 'carbs', 'fat'])
-                        elif macro.plan_name == 'Low Fat':
-                            macro.protein = lowfat_data['protein']
-                            macro.carbs = lowfat_data['carbs']
-                            macro.fat = lowfat_data['fat']
-                            macro.save(update_fields=['protein', 'carbs', 'fat'])
-                        elif macro.plan_name == 'High Protein':
-                            macro.protein = highprotein_data['protein']
-                            macro.carbs = highprotein_data['carbs']
-                            macro.fat = highprotein_data['fat']
-                            macro.save(update_fields=['protein', 'carbs', 'fat'])
-
-                    macros = {
-                        "calorie": round(response_data['calorie']),
-                        "balanced": MacroDistribution.objects.get(plan_name='Balanced', user_diet=diet),
-                        "lowcarbs": MacroDistribution.objects.get(plan_name='Low Carb', user_diet=diet),
-                        "lowfat": MacroDistribution.objects.get(plan_name='Low Fat', user_diet=diet),
-                        "highprotein": MacroDistribution.objects.get(plan_name='High Protein', user_diet=diet),
-                    }
-
-                    return render(request, "calculator/profile.html", {"macros": macros})
-
-                elif request.user.is_authenticated and not stat_instance:
-                    user_stat = form.save(commit=False)
-                    user_stat.user = request.user
-                    user_stat.save()
-
-                    balanced_data = response_data['balanced']
-                    lowcarb_data = response_data['lowcarbs']
-                    lowfat_data = response_data['lowfat']
-                    highprotein_data = response_data['highprotein']
-
-                    diet = Diet.objects.create(
-                        calorie=response_data['calorie'],
-                        stats=user_stat,
-                    )
-
-                    macros = {
-                        "calorie": round(response_data['calorie']),
-                        "balanced": MacroDistribution.objects.create(plan_name='Balanced', **balanced_data, user_diet=diet),
-                        "lowcarbs": MacroDistribution.objects.create(plan_name='Low Carb', **lowcarb_data, user_diet=diet),
-                        "lowfat": MacroDistribution.objects.create(plan_name='Low Fat', **lowfat_data, user_diet=diet),
-                        "highprotein": MacroDistribution.objects.create(plan_name='High Protein', **highprotein_data, user_diet=diet),
-                    }
-
-                    return render(request, "calculator/profile.html", {"macros": macros})
-                else:
-                    return render(request, "calculator/profile.html", {"macros": response_data})
-
-            except requests.exceptions.RequestException as e:
-                error_message = str(e)
-                return render(
-                    request, "calculator/error.html", {"error_message": error_message}
-                )
-
-    else:
-        form = form_choice()
-        context = {"form": form}
-
-        return render(request, "calculator/home.html", context)
 
 
 def user_login(request):
