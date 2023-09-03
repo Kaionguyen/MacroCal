d8791e80637882b3df71a82d06b27ff8c2715b43
Kyle Nguyen <kylenguyencs@gmail.com>
2023-09-03 14:41:57 -0700

    Add: Creation and last updated times to model

--- src/calculator/models.py
+++ src/calculator/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class UserStat(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='stats')
+    creation_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
     age = models.IntegerField()
     sex = models.CharField(max_length=6, choices=[('male', 'Male'), ('female', 'Female')])
     weight_kg = models.DecimalField(max_digits=5, decimal_places=2)
